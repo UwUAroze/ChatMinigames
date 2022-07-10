d780419c140a650c2ccca40f714fac657d9712f9
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-07-10 21:16:24 +0100

    completed most of math game

--- src/main/java/me/aroze/chatminigames/ChatMinigames.java
+++ src/main/java/me/aroze/chatminigames/ChatMinigames.java
@@ -7,6 +7,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChatMinigames extends JavaPlugin {
 
+    static int mathNum1;
+    static int mathNum2;
+    static char mathOperator;
+    static int mathAnswer;
+
     @Override
     public void onEnable() {
         getCommand("startchatgame").setExecutor(new StartChatGame());
@@ -29,6 +34,38 @@ public final class ChatMinigames extends JavaPlugin {
                 break;
 
             case 3: // Math
+
+                int temp = (int) ((Math.random()* 3));
+                switch (temp) {
+                    case 0: mathOperator = '+'; break;
+                    case 1: mathOperator = 'x'; break;
+                    case 2: mathOperator = '-'; break;
+                }
+
+                mathNum1 = (int) ((Math.random()* 70) + 5);
+                mathNum2 = (int) ((Math.random()* 70) + 5);
+                if (mathNum1 < mathNum2) {
+                    mathNum1 = mathNum2;
+                    mathNum2 = (int) ((Math.random() * mathNum1) + 1);
+                }
+
+                switch (mathOperator) {
+                    case 'x':
+                        mathNum1 = (int) ((Math.random()* 12) + 2);
+                        mathNum2 = (int) ((Math.random()* 12) + 2);
+                        mathAnswer = mathNum1 * mathNum2;
+                        break;
+                    case '+':
+                        mathAnswer = mathNum1 + mathNum2;
+                        break;
+                    case '-':
+                        mathAnswer = mathNum1 - mathNum2;
+                        break;
+                }
+
+                Bukkit.broadcastMessage(mathNum1 + " " + mathOperator + " " + mathNum2 + " = " + mathAnswer);
+
+
                 break;
         }
     }
