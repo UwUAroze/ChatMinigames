9cb3a520da9a4d6dba7f60d42e48ba6e6cfdf6b3
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-07-10 17:28:12 +0100

    started force start command

---
+++ src/main/java/me/aroze/chatminigames/command/startchatgame.java
@@ -0,0 +1,15 @@
+package me.aroze.chatminigames.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class startchatgame implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        // soon:tm:
+
+        return true;
+    }
+}
