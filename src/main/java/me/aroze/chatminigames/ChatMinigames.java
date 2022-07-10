c7203c2ca195cdd624b930ace157702de92c9df0
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-07-10 21:22:03 +0100

    organisation + chat colour util

--- src/main/java/me/aroze/chatminigames/ChatMinigames.java
+++ src/main/java/me/aroze/chatminigames/ChatMinigames.java
@@ -1,12 +1,17 @@
 package me.aroze.chatminigames;
 
 import me.aroze.chatminigames.command.StartChatGame;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class ChatMinigames extends JavaPlugin {
 
+    private static final Pattern hexPattern = Pattern.compile("&#[a-fA-F0-9]{6}");
     static int mathNum1;
     static int mathNum2;
     static char mathOperator;
@@ -19,6 +24,18 @@ public final class ChatMinigames extends JavaPlugin {
 
     @Override
     public void onDisable() {}
+
+    public static String color(String text) {
+        Matcher match = hexPattern.matcher(text);
+        while (match.find()) {
+            String color = text.substring(match.start(), match.end());
+            text = text.replace(color, ChatColor.of(color.substring(1)).toString());
+            match = hexPattern.matcher(text);
+        }
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+
 
     public static void startGame(int game) {
 
