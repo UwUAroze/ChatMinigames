5e68ab3362395461e041d4d22f84a019688984a2
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-07-13 01:31:11 +0100

    variable reset

--- src/main/java/me/aroze/chatminigames/ChatListener.java
+++ src/main/java/me/aroze/chatminigames/ChatListener.java
@@ -84,6 +84,8 @@ public class ChatListener implements Listener {
             Bukkit.getScheduler().runTask(instance, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), instance.getConfig().getString("rewards.unscramble").replace("{player}", e.getPlayer().getName())));
             if (instance.getConfig().getBoolean("misc.play-noteblock-pling")) e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, (float) instance.getConfig().getDouble("misc.noteblock-volume"), (float) instance.getConfig().getDouble("misc.noteblock-pitch"));
 
+            actualWord = "";
+            scrambledWord = "";
 
         }
 
