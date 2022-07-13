764d7d58fd7853b8f696907dfe0f7841d436f99c
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-07-13 02:02:22 +0100

    Re-round when subtracting seconds in makeTimestamp() because floating point rounding shitty shit ;-;

--- src/main/java/me/aroze/chatminigames/ChatListener.java
+++ src/main/java/me/aroze/chatminigames/ChatListener.java
@@ -94,18 +94,21 @@ public class ChatListener implements Listener {
     public String makeTimestamp(double milliseconds) {
         StringBuilder timestamp = new StringBuilder();
 
-        double seconds = Math.round(milliseconds / 10) / 100.0; // this rounds to 2dp
+        double seconds = Math.round(milliseconds / 10) / 100.0; // this rounds to 2dp, along with converting to seconds
         int minutes = (int) ((milliseconds / 1000) / 60);
 
         if (minutes == 1) {
             seconds -= 60;
+            seconds = Math.round(seconds * 100) / 100.0; // Re-round when subtracting seconds because floating point rounding shitty shit ;-;
             timestamp.append("1 minute and ");
         }
 
         else if (minutes > 0) {
             seconds -= 60 * minutes;
+            seconds = Math.round(seconds * 100) / 100.0; // Re-round when subtracting seconds because floating point rounding shitty shit ;-;
             timestamp.append(minutes).append(" minutes and ");
         }
+
         timestamp.append(seconds).append(" seconds");
 
         return timestamp.toString();
