70848feb25d76869ae3983446b0196dd9d630fba
UwUAroze <admin@aroze.me>
2023-04-15 14:57:06 -0400

    Debuggy wuggy

--- src/main/kotlin/me/aroze/chatminigames/command/TestCommand.kt
+++ src/main/kotlin/me/aroze/chatminigames/command/TestCommand.kt
@@ -4,16 +4,16 @@ import me.aroze.arozeutils.minecraft.command.CommandInfo
 import me.aroze.arozeutils.minecraft.command.FancyCommand
 import me.aroze.chatminigames.ChatMinigames.Companion.randomisedWords
 import me.aroze.chatminigames.minigame.GenericGame
+import me.aroze.chatminigames.minigame.impl.RushGame
 import org.bukkit.command.CommandSender
 
 @CommandInfo(
     description = "testy test",
-    permission = "chatminigames.craft",
+    permission = "chatminigames.test",
     permissionMessage = "No testy for you!",
 )
-object TestCommand : FancyCommand("testgame") {
+object TestCommand : FancyCommand("testrush") {
     override fun onCommand(sender: CommandSender, label: String, args: Array<out String>) {
-        val game = GenericGame("testy", randomisedWords.next().toString())
-        game.start()
+        val game = RushGame.create()
     }
 }
